e8f00f8e7a85a1922a6e15727a0b652913fd0598
Emily Platzer <emilyplatzer@gmail.com>
2018-12-07 19:30:46 -0700

    Day 2 part one

---
+++ spec/d02-inventoryManagementSystem.spec.js
@@ -0,0 +1,19 @@
+// https://adventofcode.com/2018/day/2
+
+
+const inventoryManagementSystem = require('../lib/d02-inventoryManagementSystem');
+
+describe('inventoryManagementSystem', () => {
+  it('returns 0 when given "abcdef\n"', () => {
+    expect(inventoryManagementSystem("abcdef\n")).toBe(0);
+  });
+  it('returns 1 when given "abcdef\nbababc\n"', () => {
+    expect(inventoryManagementSystem("abcdef\nbababc\n")).toBe(1);
+  });
+  it('returns 2 when given "abcdef\nbababc\nabbcde\n"', () => {
+    expect(inventoryManagementSystem("abcdef\nbababc\nabbcde\n")).toBe(2);
+  });
+  it('returns 12 when given "abcdef\nbababc\nabbcde\nabcccd\naabcdd\nabcdee\nababab"', () => {
+    expect(inventoryManagementSystem("abcdef\nbababc\nabbcde\nabcccd\naabcdd\nabcdee\nababab")).toBe(12);
+  });
+});
