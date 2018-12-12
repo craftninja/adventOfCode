9cb0a3e1cd92e8c48ded456e5c5ecb208128e95f
Emily Platzer <emilyplatzer@gmail.com>
2018-12-12 12:07:23 -0700

    Added test for overlapped 3x

--- spec/d03-noMatterHowYouSliceIt.spec.js
+++ spec/d03-noMatterHowYouSliceIt.spec.js
@@ -16,4 +16,8 @@ describe('cuttingDeficits', () => {
     const cuttingOrders = "#1 @ 1,3: 4x4\n #2 @ 1,3: 4x4\n #3 @ 1,3: 4x4";
     expect(cuttingDeficits(cuttingOrders)).toBe(16);
   });
+  it('returns 4 when given "#1 @ 1,3: 4x4\n #2 @ 3,1: 4x4\n #3 @ 5,5: 2x2\n #4 @ 3,3: 2x2\n"', () => {
+    const cuttingOrders = "#1 @ 1,3: 4x4\n #2 @ 3,1: 4x4\n #3 @ 5,5: 2x2\n #4 @ 3,3: 2x2\n";
+    expect(cuttingDeficits(cuttingOrders)).toBe(4);
+  });
 });
