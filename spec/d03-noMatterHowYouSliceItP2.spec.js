9609a0a8486a6b65174037510085b69ffa0a94ac
Emily Platzer <emilyplatzer@gmail.com>
2018-12-14 14:49:54 -0700

    Day 3 part 2

---
+++ spec/d03-noMatterHowYouSliceItP2.spec.js
@@ -0,0 +1,15 @@
+// https://adventofcode.com/2018/day/3
+
+
+const noOverlaps = require('../lib/d03-noMatterHowYouSliceItP2');
+
+describe('noOverlaps', () => {
+  it('returns undefined when given "#1 @ 1,3: 4x4\n#2 @ 1,3: 4x4"', () => {
+    const cuttingOrders = "#1 @ 1,3: 4x4\n#2 @ 1,3: 4x4";
+    expect(noOverlaps(cuttingOrders)).toBe(undefined);
+  });
+  it('returns 3 when given "#1 @ 1,3: 4x4\n#2 @ 1,3: 4x4\n#3 @ 10,3: 4x4"', () => {
+    const cuttingOrders = "#1 @ 1,3: 4x4\n#2 @ 1,3: 4x4\n#3 @ 10,3: 4x4";
+    expect(noOverlaps(cuttingOrders)).toBe(3);
+  });
+});
