ac59471556ce81a218b94789fc93852e67cd2b1f
Emily Platzer <emilyplatzer@gmail.com>
2024-10-17 12:38:16 -0600

    Mild refactoring

--- spec/2023/d01-Trebuchet.spec.js
+++ spec/2023/d01-Trebuchet.spec.js
@@ -6,35 +6,35 @@ const trebuchet = require('../../lib/2023/d01-trebuchet');
 describe('trebuchet', () => {
   describe('can handle one line', () => {
     it('returns 12 when given "1abc2"', () => {
-      const report = "1abc2";
-      expect(trebuchet(report)).toBe(12);
+      const messedCalibrationValue = "1abc2";
+      expect(trebuchet(messedCalibrationValue)).toBe(12);
     });
     it('returns 38 when given "pqr3stu8vwx"', () => {
-      const report = "pqr3stu8vwx";
-      expect(trebuchet(report)).toBe(38);
+      const messedCalibrationValue = "pqr3stu8vwx";
+      expect(trebuchet(messedCalibrationValue)).toBe(38);
     });
     it('returns 15 when given "a1b2c3d4e5f"', () => {
-      const report = "a1b2c3d4e5f";
-      expect(trebuchet(report)).toBe(15);
+      const messedCalibrationValue = "a1b2c3d4e5f";
+      expect(trebuchet(messedCalibrationValue)).toBe(15);
     });
     it('returns 77 when given "treb7uchet"', () => {
-      const report = "treb7uchet";
-      expect(trebuchet(report)).toBe(77);
+      const messedCalibrationValue = "treb7uchet";
+      expect(trebuchet(messedCalibrationValue)).toBe(77);
     });
   })
 
   describe('can handle multiple lines', () => {
     it('returns 50 when given "1abc2\npqr3stu8vwx"', () => {
-      const report = "1abc2\npqr3stu8vwx";
-      expect(trebuchet(report)).toBe(50);
+      const messedCalibrationValues = "1abc2\npqr3stu8vwx";
+      expect(trebuchet(messedCalibrationValues)).toBe(50);
     });
     it('returns 65 when given "1abc2\npqr3stu8vwx\na1b2c3d4e5f"', () => {
-      const report = "1abc2\npqr3stu8vwx\na1b2c3d4e5f";
-      expect(trebuchet(report)).toBe(65);
+      const messedCalibrationValues = "1abc2\npqr3stu8vwx\na1b2c3d4e5f";
+      expect(trebuchet(messedCalibrationValues)).toBe(65);
     });
     it('returns 142 when given "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet"', () => {
-      const report = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet";
-      expect(trebuchet(report)).toBe(142);
+      const messedCalibrationValues = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet";
+      expect(trebuchet(messedCalibrationValues)).toBe(142);
     });
   })
 });
