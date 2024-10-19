d050b8e0e750bc6e9749da8e4e1be9eeb788b234
Emily Platzer <emilyplatzer@gmail.com>
2024-10-19 13:50:53 -0600

    can handle EoF newline

--- spec/2023/d02-CubeConundrum.spec.js
+++ spec/2023/d02-CubeConundrum.spec.js
@@ -34,5 +34,39 @@ describe('support function', () => {
       })
 
     })
+    it('can parse two game records "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"', () => {
+      const rawGameRecords = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue";
+      const gameRecords = cubeConundrum.parseGameRecords(rawGameRecords);
+      expect(gameRecords).toEqual({
+        "1": [
+          {"blue": 3, "red": 4},
+          {"red": 1, "green": 2, "blue": 6},
+          {"green": 2},
+        ],
+        "2": [
+          {"blue": 1, "green": 2},
+          {"green": 3, "blue": 4, "red": 1},
+          {"green": 1, "blue": 1},
+        ]
+      })
+
+    })
+    it('can parse two game records with trailing newline "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n"', () => {
+      const rawGameRecords = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n";
+      const gameRecords = cubeConundrum.parseGameRecords(rawGameRecords);
+      expect(gameRecords).toEqual({
+        "1": [
+          {"blue": 3, "red": 4},
+          {"red": 1, "green": 2, "blue": 6},
+          {"green": 2},
+        ],
+        "2": [
+          {"blue": 1, "green": 2},
+          {"green": 3, "blue": 4, "red": 1},
+          {"green": 1, "blue": 1},
+        ]
+      })
+
+    })
   })
 })
