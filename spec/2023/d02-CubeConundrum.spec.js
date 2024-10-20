5f014722b09f505f65e13a2de6c449de649c7375
Emily Platzer <emilyplatzer@gmail.com>
2024-10-20 12:23:30 -0600

    2023 day 2 cubeconundrum can calculate sum of possible game ids

--- spec/2023/d02-CubeConundrum.spec.js
+++ spec/2023/d02-CubeConundrum.spec.js
@@ -3,7 +3,33 @@
 
 const cubeConundrum = require('../../lib/2023/d02-cubeConundrum');
 
-describe('support function', () => {
+describe('cubeConundrum', () => {
+  describe('can handle one line', () => {
+    it('returns 1 when given "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"', () => {
+      const gameRecord = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+      const bagContents = "12 red, 13 green, 14 blue"
+      expect(cubeConundrum.calculateSum(bagContents, gameRecord)).toBe(1);
+    });
+  })
+
+  describe('can handle multiple lines', () => {
+    it('returns 8 when given "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"', () => {
+      const gameRecords = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+      const bagContents = "12 red, 13 green, 14 blue"
+      expect(cubeConundrum.calculateSum(bagContents, gameRecords)).toBe(8);
+    });
+  })
+
+  describe('can handle empty line at end of input', () => {
+    it('returns 8 when given "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"', () => {
+      const gameRecords = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green\n";
+      const bagContents = "12 red, 13 green, 14 blue"
+      expect(cubeConundrum.calculateSum(bagContents, gameRecords)).toBe(8);
+    });
+  })
+});
+
+describe('support functions', () => {
   describe('parseCubeSet', () => {
     it('can parse a list of colored cubes "12 red, 13 green, 14 blue"', () => {
       const rawBagContents = "12 red, 13 green, 14 blue"
