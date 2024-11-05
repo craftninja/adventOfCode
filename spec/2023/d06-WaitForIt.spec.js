520a7819a4852a240222410de40e21d0b333969f
Emily Platzer <emilyplatzer@gmail.com>
2024-11-05 16:44:31 -0700

    2023 d06 in progress, support apps maybe complete?

---
+++ spec/2023/d06-WaitForIt.spec.js
@@ -0,0 +1,52 @@
+// https://adventofcode.com/2023/day/6
+
+const rawRaceResults = "Time:      7  15   30\nDistance:  9  40  200"
+
+const waitForIt = require('../../lib/2023/d06-waitForIt');
+
+describe('waitForIt', () => {
+  xdescribe('closestLocationOfSeedSite2', () => {
+    it(`returns 288 when given \n"${rawRaceResults}"`, () => {
+      expect(waitForIt.productOfNumberOfWaysToWinForEachRace(rawRaceResults)).toBe(288);
+    });
+    it(`returns 288 when given \n"${rawRaceResults + '\n'}"`, () => {
+      expect(waitForIt.productOfNumberOfWaysToWinForEachRace(rawRaceResults + "\n")).toBe(288);
+    });
+  })
+
+  describe('support functions', () => {
+    describe('raceResults', () => {
+      it('can create race result object from raw race result data', () => {
+        expect(
+          waitForIt.raceResults(rawRaceResults)
+        ).toEqual([
+          { time: 7, distance: 9 },
+          { time: 15, distance: 40 },
+          { time: 30, distance: 200 },
+        ])
+      })
+      it('returns error with unequal time and distance data', () => {
+        expect(
+          () => waitForIt.raceResults(rawRaceResults + "   400")
+        ).toThrowError("Time and Distance data unequal")
+      })
+    })
+    describe('waysToWin', () => {
+      it('can create `ways to win` object from race result object', () => {
+        const raceResults = [
+          { time: 7, distance: 9 },
+          { time: 15, distance: 40 },
+          { time: 30, distance: 200 },
+        ]
+        expect(waitForIt.waysToWin(raceResults)).toEqual(
+          [ 4, 8, 9 ]
+        )
+      })
+    })
+    describe('productOfArray', () => {
+      it('gives the product of numbers in an array', () => {
+        expect(waitForIt.productOfArray([ 4, 8, 9 ])).toEqual(288)
+      })
+    })
+  })
+});
