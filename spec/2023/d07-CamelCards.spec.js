123a73c944d4247213e99fb4a145333c181d3cf2
Emily Platzer <emilyplatzer@gmail.com>
2024-11-12 18:08:27 -0700

    2023 d07p2

--- spec/2023/d07-CamelCards.spec.js
+++ spec/2023/d07-CamelCards.spec.js
@@ -13,6 +13,14 @@ describe('camelCards', () => {
       expect(camelCards.calculateWinnings(rawCardData + "\n")).toBe(6440);
     });
   });
+  describe('calculateWinningsJWild', () => {
+    it(`returns 5905 when given \n"${rawCardData}"`, () => {
+      expect(camelCards.calculateWinningsJWild(rawCardData)).toBe(5905);
+    });
+    it(`returns 5905 when given \n"${rawCardData + '\n'}"`, () => {
+      expect(camelCards.calculateWinningsJWild(rawCardData + "\n")).toBe(5905);
+    });
+  });
 
   describe('support functions', () => {
     describe('parseCardData', () => {
@@ -73,9 +81,81 @@ describe('camelCards', () => {
           fiveOfAKind: [{hand: '33333', bid: 1}],
         })
       })
+      it('can create lists of hand types from card data with J Wild', () => {
+        const cardData = camelCards.parseCardData(rawCardData)
+        expect(
+          camelCards.handTypesJWild(cardData)
+        ).toEqual({
+          highCard: [],
+          onePair: [{hand: '32T3K', bid: 765 }],
+          twoPair: [{hand: 'KK677', bid: 28 }],
+          threeOfAKind: [],
+          fullHouse: [],
+          fourOfAKind: [
+            {hand: 'T55J5', bid: 684 },
+            {hand: 'KTJJT', bid: 220 },
+            {hand: 'QQQJA', bid: 483 },
+          ],
+          fiveOfAKind: [],
+        })
+      })
+      it('can create all hand types from card data with J wild', () => {
+        const cardData = [
+          {hand: '55555', bid: 5 },
+          {hand: '5555J', bid: 5 },
+          {hand: '555JJ', bid: 5 },
+          {hand: '4444K', bid: 4 },
+          {hand: '444JK', bid: 4 },
+          {hand: '44JJK', bid: 4 },
+          {hand: '4JJJK', bid: 4 },
+          {hand: '99966', bid: 9 },
+          {hand: '99J66', bid: 9 },
+          {hand: '33396', bid: 3 },
+          {hand: '33J96', bid: 3 },
+          {hand: '3JJ96', bid: 3 },
+          {hand: '22446', bid: 4 },
+          {hand: '22468', bid: 2 },
+          {hand: '2J468', bid: 2 },
+          {hand: '24689', bid: 1 },
+        ]
+        expect(
+          camelCards.handTypesJWild(cardData)
+        ).toEqual({
+          highCard: [
+            {hand: '24689', bid: 1 },
+          ],
+          onePair: [
+            {hand: '22468', bid: 2 },
+            {hand: '2J468', bid: 2 },
+          ],
+          twoPair: [
+            {hand: '22446', bid: 4 },
+          ],
+          threeOfAKind: [
+            {hand: '33396', bid: 3 },
+            {hand: '33J96', bid: 3 },
+            {hand: '3JJ96', bid: 3 },
+          ],
+          fullHouse: [
+            {hand: '99966', bid: 9 },
+            {hand: '99J66', bid: 9 },
+          ],
+          fourOfAKind: [
+            {hand: '4444K', bid: 4 },
+            {hand: '444JK', bid: 4 },
+            {hand: '44JJK', bid: 4 },
+            {hand: '4JJJK', bid: 4 },
+          ],
+          fiveOfAKind: [
+            {hand: '55555', bid: 5 },
+            {hand: '5555J', bid: 5 },
+            {hand: '555JJ', bid: 5 },
+          ],
+        })
+      })
     })
     describe('rankHands', () => {
-      it('can create rank object from simple cardData, handTypes', () => {
+      it('can create rank object from simple handTypes', () => {
         const cardData = camelCards.parseCardData(
           "33333 1\n33339 2\n33399 3\n33369 4\n33669 5\n33169 6\n1369Q 7"
         );
@@ -92,7 +172,7 @@ describe('camelCards', () => {
           { hand: '33333', bid: 1, rank: 7},
         ])
       })
-      it('can create rank object from cardData, handTypes', () => {
+      it('can create rank object from handTypes', () => {
         const cardData = camelCards.parseCardData(rawCardData);
         const handTypes = camelCards.handTypes(cardData)
         expect(
@@ -103,6 +183,38 @@ describe('camelCards', () => {
           { hand: 'KK677', bid: 28, rank: 3},
           { hand: 'T55J5', bid: 684, rank: 4},
           { hand: 'QQQJA', bid: 483, rank: 5},
+        ])
+      })
+      it('can create rank object from handTypes with J Wild', () => {
+        const cardData = camelCards.parseCardData(rawCardData);
+        const handTypes = camelCards.handTypesJWild(cardData)
+        expect(
+          camelCards.rankHands(handTypes)
+        ).toEqual([
+          { hand: '32T3K', bid: 765, rank: 1},
+          { hand: 'KK677', bid: 28, rank: 2},
+          { hand: 'T55J5', bid: 684, rank: 3},
+          { hand: 'QQQJA', bid: 483, rank: 4},
+          { hand: 'KTJJT', bid: 220, rank: 5},
+        ])
+      })
+      it('can create rank object with J as least rank with J Wild', () => {
+        expect(
+          camelCards.rankHandsJWild({
+            highCard: [],
+            onePair: [],
+            twoPair: [],
+            threeOfAKind: [],
+            fullHouse: [],
+            fourOfAKind: [
+              {hand: '88882', bid: 1},
+              {hand: 'JKKK2', bid: 2},
+            ],
+            fiveOfAKind: [],
+          })
+        ).toEqual([
+          { hand: 'JKKK2', bid: 2, rank: 1},
+          { hand: '88882', bid: 1, rank: 2},
         ])
       })
     })
