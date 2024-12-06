19c9d45d10a879811a97cdd34862485fe4e95737
Emily Platzer <emilyplatzer@gmail.com>
2024-12-05 18:10:40 -0700

    2024 d03p2

--- spec/2024/d03-MullItOver.spec.js
+++ spec/2024/d03-MullItOver.spec.js
@@ -1,6 +1,8 @@
 // https://adventofcode.com/2024/day/3
 
 const rawCorruptedMemory = 'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))'
+const rawCorruptedMemory2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+const rawCorruptedMemory3 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
 const mullItOver = require('../../lib/2024/d03-mullItOver');
 
@@ -13,6 +15,14 @@ describe('mullItOver', () => {
       expect(mullItOver.mulSumCorruptedMemory(rawCorruptedMemory + "\n")).toBe(161);
     });
   });
+  describe('mulSumCorruptedMemoryFollowingInstructions', () => {
+    it(`returns 48 when given \n"${rawCorruptedMemory2}"`, () => {
+      expect(mullItOver.mulSumCorruptedMemoryFollowingInstructions(rawCorruptedMemory2)).toBe(48);
+    });
+    it(`returns 48 when given \n"${rawCorruptedMemory2 + '\n'}"`, () => {
+      expect(mullItOver.mulSumCorruptedMemoryFollowingInstructions(rawCorruptedMemory2 + "\n")).toBe(48);
+    });
+  });
 
   describe('support functions', () => {
     describe('parsedMulInstructions', () => {
@@ -23,6 +33,28 @@ describe('mullItOver', () => {
           [2, 4],
           [5, 5],
           [11, 8],
+          [8, 5],
+        ])
+      })
+    })
+    describe('parsedMulFollowingInstructions', () => {
+      it('returns parsed mul instructions from a raw corrupted memory', () => {
+        expect(
+          mullItOver.parsedMulFollowingInstructions(rawCorruptedMemory2)
+        ).toEqual([
+          [2, 4],
+          [8, 5],
+        ])
+      })
+    })
+    describe('parsedMulFollowingInstructions', () => {
+      it('returns parsed mul instructions (2x) from a raw corrupted memory', () => {
+        expect(
+          mullItOver.parsedMulFollowingInstructions(rawCorruptedMemory3)
+        ).toEqual([
+          [2, 4],
+          [8, 5],
+          [2, 4],
           [8, 5],
         ])
       })
